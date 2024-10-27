eabe05e532c85b29d4f80b06db43377dafc7a6c4
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-10-27 21:52:04 +0300

    Project Finish

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,7 @@ import { UserButton, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import HeaderLogo from "@/components/header-logo";
 import Navigation from "@/components/navigation";
 import WelcomeMsg from "@/components/welcome-msg";
+import { Filters } from "./filters";
 
 export const Header = () => {
   return (
@@ -22,6 +23,7 @@ export const Header = () => {
           </ClerkLoading>
         </div>
         <WelcomeMsg />
+        <Filters />
       </div>
     </header>
   );
